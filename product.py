e4e2e640f58d3b8fedf1b6f4336ac522fccc02e3
Lik Eduard <feft99@gmail.com>
2021-07-02 05:30:35 +0600

    created Product in product.py

---
+++ product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, brand: str, title: str, price: str, discount: str, link: str):
+        self.brand = brand
+        self.title = title
+        self.price = price
+        self.discount = discount
+        self.link = link
