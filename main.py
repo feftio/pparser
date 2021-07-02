df321f0c50aef62283b65b2e806af75b6529ef98
Lik Eduard <feft99@gmail.com>
2021-07-02 13:43:58 +0600

    example with couple pages

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from product_parser import ProductParser
 url = 'https://www.wildberries.ru/catalog/obuv/muzhskaya/kedy-i-krossovki?sort=priceup&xsubject=128&page=1'
 
 parser = ProductParser(url=url)
-products = parser.get_products({
-    'page': 8
-})
 
-print(len(products))
+pages = []
+for page in range(1, 9):
+    products = parser.get_products({
+    'page': page
+    })
+    pages.append(products)
+
+print(len(pages))
