053cd2ee4a31441c66679b851292a0d8d1bce3ad
Lik Eduard <feft99@gmail.com>
2021-07-05 05:40:41 +0600

    more options in PParser

--- pparser/main.py
+++ pparser/main.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import typing as t
 
-from copy import deepcopy
+from pyppeteer import browser
+
 from pparser.url_parser import UrlParser
 
 from bs4 import BeautifulSoup
@@ -12,42 +13,59 @@ from pyppeteer.browser import Browser
 from urllib.parse import ParseResult, urlparse, parse_qsl, urlencode, urlunparse
 from pparser.product import Product
 
+from pparser.utils import merge_queries
 
-async def launch_browser():
-    return await launch(headless=True, ignoreHTTPSErrors=False, args=[
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-setuid-sandbox',
-        '--no-first-run',
-        '--no-sandbox',
-        '--no-zygote',
-        '--deterministic-fetch',
-        '--disable-features=IsolateOrigins',
-        '--disable-site-isolation-trials'
-    ],)
+LAUNCH_ARGS = (
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+    '--deterministic-fetch',
+    '--disable-features=IsolateOrigins',
+    '--disable-site-isolation-trials',
+)
+
+DEFAULT_HEADERS = {}
 
 
-class PParser(UrlParser):
+def run_async(awaitable):
+    return asyncio.get_event_loop().run_until_complete(awaitable)
 
-    def __init__(self, url: str, verify: bool = True, visible: bool = False):
-        super().__init__(url)
-        self.browser: Browser = asyncio.get_event_loop().run_until_complete(launch_browser())
 
-    async def get_content(self, url: str, selector: t.Optional[str] = None):
+class PParser:
+
+    def __init__(self, url: str, verify: bool = True, visible: bool = False, width: int = 1920, height: int = 1080):
+        self._url_parser = UrlParser(url)
+        self.width = width
+        self.height = height
+        self.browser: Browser = run_async(
+            launch(headless=not(visible), ignoreHTTPSErrors=not(verify), args=(
+                *LAUNCH_ARGS, f'--window-size={self.width},{self.height}')))
+
+    async def get_content(self, url: str, waitable_selector: t.Optional[str] = None, timeout: int = 4000, sleep: float = 0.2):
         page = await self.browser.newPage()
-        await page.goto(url, options={'timeout': int(8 * 1000)})
-        if not(selector is None):
-            await page.waitForSelector(selector)
+        await page.setViewport({
+            'width': self.width,
+            'height': self.height,
+        })
+        await page.goto(url, options={'timeout': int(timeout)})
+        if waitable_selector is not None:
+            await page.waitForSelector(waitable_selector)
+        await asyncio.sleep(sleep)
+        await page.screenshot({'path': 'screenshot.png'})
         return await page.content()
 
-    def get_products(self, query: t.Dict[str, t.Any] = {}, wait_for_selector: t.Optional[str] = None) -> t.List[Product]:
-        _query = urlencode({**dict(parse_qsl(self.query)), **query})
-        _url = self.copywith(query=_query)
+    def get_products(self, query: t.Dict[str, t.Any] = {}, selector: str = '', timeout: int = 4000, sleep: float = 0.2) -> t.List[Product]:
+        _url_parser = self._url_parser.copywith(
+            query=merge_queries(self._url_parser.query, query))
 
         products = []
 
-        soup = BeautifulSoup(asyncio.get_event_loop().run_until_complete((self.get_content(url=_url.full_url, selector=wait_for_selector))), 'html.parser')
-        soup_products = soup.findAll('div', class_='dtList-inner')
+        soup = BeautifulSoup(run_async((self.get_content(
+            url=_url_parser.full_url, waitable_selector=selector, timeout=timeout, sleep=sleep))), 'html.parser')
+        soup_products = soup.select(selector)
 
         for soup_product in soup_products:
             discount = soup_product.find('span', class_='price-sale active')
@@ -62,7 +80,7 @@ class PParser(UrlParser):
                     price=soup_product.find(
                         class_='lower-price').get_text(strip=True).replace('\xa0', '').replace('₽', ''),
                     discount=discount,
-                    link=_url.root_url +
+                    link=_url_parser.root_url +
                     soup_product.find(
                         'a', class_='ref_goods_n_p j-open-full-product-card').get('href')
                 )
