aa26a8d3c1247d1e3b5d7a9a665e790b75f90190
Lik Eduard <feft99@gmail.com>
2021-07-06 20:45:40 +0600

    + get_soups(), + _pbrowser

--- pparser/main.py
+++ pparser/main.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from pparser.pbrowser import PBrowser
 import typing as t
-import asyncio
 
 from bs4 import BeautifulSoup
 from pyppeteer import launch
@@ -11,41 +10,39 @@ from pparser.product import Product
 from pparser.utils import merge_queries
 from pparser.url_parser import UrlParser
 
-from pparser.utils import run_async
-
 
 class PParser:
 
-    def __init__(self, url: str, **settings):
+    def __init__(self, url: str, **options):
         self._url_parser = UrlParser(url)
-        self._modern_broser = PBrowser(**settings)
+        self._pbrowser = PBrowser(**options)
 
-    def get_products(self, query: t.Dict[str, t.Any] = {}, selector: str = '', timeout: int = 4000, sleep: float = 0.2) -> t.List[Product]:
+    def get_soups(self, query: t.Dict[str, t.Any] = {}, waitable_selector: t.Optional[str] = None, selector: str = '', timeout: int = 6000, sleep: float = 0.2) -> t.List[Product]:
         _url_parser = self._url_parser.copywith(
             query=merge_queries(self._url_parser.query, query))
 
-        products = []
-
-        soup = BeautifulSoup(run_async((self.get_content(
-            url=_url_parser.full_url, waitable_selector=selector, timeout=timeout, sleep=sleep))), 'html.parser')
+        soup = BeautifulSoup((self._pbrowser.get_content(
+            url=_url_parser.full_url, waitable_selector=waitable_selector, timeout=timeout, sleep=sleep)), 'html.parser')
         soup_products = soup.select(selector)
 
-        for soup_product in soup_products:
-            discount = soup_product.find('span', class_='price-sale active')
-            discount = discount.get_text(strip=True) if discount else ''
+        return soup_products
 
-            products.append(
-                Product(
-                    brand=soup_product.find(
-                        'strong', class_='brand-name c-text-sm').get_text(strip=True).replace('/', ''),
-                    title=soup_product.find(
-                        'span', class_='goods-name c-text-sm').get_text().split('/')[0],
-                    price=soup_product.find(
-                        class_='lower-price').get_text(strip=True).replace('\xa0', '').replace('₽', ''),
-                    discount=discount,
-                    link=_url_parser.root_url +
-                    soup_product.find(
-                        'a', class_='ref_goods_n_p j-open-full-product-card').get('href')
-                )
-            )
-        return products
+        # for soup_product in soup_products:
+        #     discount = soup_product.find('span', class_='price-sale active')
+        #     discount = discount.get_text(strip=True) if discount else ''
+
+        #     products.append(
+        #         Product(
+        #             brand=soup_product.find(
+        #                 'strong', class_='brand-name c-text-sm').get_text(strip=True).replace('/', ''),
+        #             title=soup_product.find(
+        #                 'span', class_='goods-name c-text-sm').get_text().split('/')[0],
+        #             price=soup_product.find(
+        #                 class_='lower-price').get_text(strip=True).replace('\xa0', '').replace('₽', ''),
+        #             discount=discount,
+        #             link=_url_parser.root_url +
+        #             soup_product.find(
+        #                 'a', class_='ref_goods_n_p j-open-full-product-card').get('href')
+        #         )
+        #     )
+        # return products
