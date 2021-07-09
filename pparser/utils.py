79c3d6f1c47e527506b287cc18d6dc079b0c39a6
Lik Eduard <feft99@gmail.com>
2021-07-09 18:49:23 +0600

    refact merge_options -> merge_dicts() + utils has __all__

--- pparser/utils.py
+++ pparser/utils.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 import typing as t
 from urllib.parse import parse_qsl, urlencode
-from collections import defaultdict
 import asyncio
+
+__all__ = [
+    'stringify_query',
+    'merge_queries',
+    'merge_dicts',
+    'run_async'
+]
 
 
 def stringify_query(query: t.Union[str, t.Dict[str, t.Any]]) -> str:
@@ -20,18 +26,22 @@ def merge_queries(queryb: t.Union[str, t.Dict[str, t.Any]], querys: t.Union[str,
     return urlencode({**dict(parse_qsl(queryb)), **dict(parse_qsl(querys))})
 
 
-def merge_options(*options: t.Dict[t.Any, t.Any]) -> t.Dict[t.Any, t.Aby]:
-    options = list(options)
-    sc = defaultdict(lambda *args: args, options.pop(0))
-    for _ in range(len(options)):
-        sn = options.pop(0)
-        for k, v in sn.items():
-            if isinstance(sc[k], list):
-                sc[k].extend(v)
-                sc[k] = list(set(sc[k]))
+def merge_dicts(*dicts, expandable: bool = False):
+    dicts = list(dicts)
+    c = dicts.pop(0)
+    for _ in range(len(dicts)):
+        n = dicts.pop(0)
+        for k in n.keys():
+            if k in c:
+                if isinstance(c[k], list):
+                    c[k].extend(n[k])
+                    c[k] = list(set(c[k]))
+                else:
+                    c[k] = n[k]
             else:
-                sc[k] = v
-    return dict(sc)
+                if expandable:
+                    c[k] = n[k]
+    return dict(c)
 
 
 def run_async(awaitable):
