03f31349b67ccc83a914ddbbe34d2dc524355879
Lik Eduard <feft99@gmail.com>
2021-07-20 15:27:18 +0600

    example.py change

--- examples/example.py
+++ examples/example.py
@@ -3,15 +3,14 @@ import pparser
 # https://www.wildberries.ru/catalog/obuv/muzhskaya/kedy-i-krossovki?sort=priceup&xsubject=128&page=1
 # https://www.wildberries.ru/catalog/0/search.aspx?xfilters=xsubject%3Bdlvr%3Bbrand%3Bprice%3Bkind%3Bcolor%3Bwbsize%3Bseason%3Bconsists&xparams=preset%3D10948169&xshard=presets%2Fbucket_114&page=1&search=%D0%A1%D0%B8%D0%BC%D0%BF%D0%BB+%D0%B4%D0%B8%D0%BC%D0%BF%D0%BB&xsubject=1065%3B297%3B227&utm_source=vkentryprofit&click_id=v1_307801332
 
-url = 'https://www.wildberries.ru/catalog/obuv/muzhskaya/kedy-i-krossovki?sort=priceup&xsubject=128&page=1'
+url = 'https://www.wildberries.ru/'
 pp = pparser.PParser(url=url)
-pp.set_options()
 for item in pp.select(selector='div.dtList-inner'):
-    print(item.select_one('strong.brand-name.c-text-sm').string)  # brand
+    print(item.select_one('strong.brand-name.c-text-sm').text)  # brand
     print(item.select_one('span.goods-name.c-text-sm').string)  # title
     print(item.select_one('.lower-price').string)  # price
     print(item.select_one('span.price-sale.active').string)  # discount
-    print(item.select_one('a.ref_goods_n_p.j_open_full-product-card').href)  # link
+    # print(item.select_one('a.ref_goods_n_p.j_open_full-product-card').href)  # link
     print('---------------------------')
 
 # for soup_product in soup_products:
